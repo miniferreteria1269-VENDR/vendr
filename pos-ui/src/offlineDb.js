829457d676eeb031b74f3b7a58e189d7872c9508
miniferreteria1269-VENDR <miniferreteria1269@gmail.com>
2026-07-21 10:30:57 -0600

    Update offlineDb.js

--- pos-ui/src/offlineDb.js
+++ pos-ui/src/offlineDb.js
@@ -3,5 +3,14 @@ import Dexie from "dexie";
 export const offlineDb = new Dexie("vendr_offline");
 
 offlineDb.version(1).stores({
-  pendingSales: "client_event_id, store_id, status, created_at"
+  pendingSales:
+    "client_event_id, store_id, status, created_at"
+});
+
+offlineDb.version(2).stores({
+  pendingSales:
+    "client_event_id, store_id, status, created_at",
+
+  products:
+    "[store_id+product_id], store_id, product_id, name, is_active"
 });
