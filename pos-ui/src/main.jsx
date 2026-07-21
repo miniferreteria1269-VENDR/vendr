3b32a2dbab0c34da15ed80c5b40337d64caad742
miniferreteria1269-VENDR <miniferreteria1269@gmail.com>
2026-07-21 12:27:02 -0600

    Update main.jsx

--- pos-ui/src/main.jsx
+++ pos-ui/src/main.jsx
@@ -1,14 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App.jsx";
 import { LanguageProvider } from "./LanguageContext";
 
-<LanguageProvider>
-  <App />
-</LanguageProvider>
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LanguageProvider>
       <App />
