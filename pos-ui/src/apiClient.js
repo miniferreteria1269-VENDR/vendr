11e8bccc8c99338f2e9db90834ba4777134e1b51
miniferreteria1269-VENDR <miniferreteria1269@gmail.com>
2026-07-25 09:58:44 -0600

    Create apiClient.js

---
+++ pos-ui/src/apiClient.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API =
+  "https://vendr-onkr.onrender.com";
+
+const apiClient = axios.create({
+  baseURL: API
+});
+
+apiClient.interceptors.request.use(
+  config => {
+    const token =
+      localStorage.getItem(
+        "vendr_access_token"
+      );
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+export default apiClient;
